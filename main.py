b5a4ab02943d2354d433e385dff4e595e3d7defc
Valerio Morelli <valeriomorelli50@gmail.com>
2022-09-02 16:42:21 +0200

    small change

--- main.py
+++ main.py
@@ -1,38 +1,30 @@
 import time
 from types import NoneType
 
-import pyautogui
+from pyautogui import moveTo,scroll,sleep,click,hotkey,locateOnScreen
 
 
 def wait_for_element_appear(element_path: str, wait=8.0, confidence=0.7, grayscale=False):
     start = time.time()
     while True:
-        element = pyautogui.locateOnScreen(element_path, grayscale=grayscale, confidence=confidence)
+        element = locateOnScreen(element_path, grayscale=grayscale, confidence=confidence)
         if time.time() - start > wait:
-            print(f"I've waited too much! [{element_path}]")
+            # print(f"I've waited too much! [{element_path}]")
             return None
-        pyautogui.sleep(0.5)
+        sleep(0.5)
         if type(element) is not NoneType:
             break
     return element
 
 
 def click_center(element):
-    pyautogui.click(x=element.left + int(element.width / 2),
+    click(x=element.left + int(element.width / 2),
                     y=element.top + int(element.height / 2))
 
 def position_mouse(element):
-    pyautogui.moveTo(x=element.left + int(element.width / 2),
+    moveTo(x=element.left + int(element.width / 2),
                     y=element.top + int(element.height / 2))
 
-
-
-def click_first_found(el_list: [], wait: float, confidence=0.9, grayscale=False):
-    for el in el_list:
-        element = wait_for_element_appear(el, wait=wait, confidence=confidence, grayscale=grayscale)
-        if type(element) is not NoneType:
-            click_center(element)
-            return
 
 
 def main():
@@ -49,16 +41,18 @@ def main():
         if (type(watch) == NoneType):
             watching = wait_for_element_appear('ui\\+watching.png', 2, confidence=0.9)
             if (type(watching) == NoneType):
-                pyautogui.hotkey('alt','left')
-                pyautogui.sleep(2)
+                hotkey('alt','left')
+                sleep(2)
                 break
             position_mouse(watching)
-            pyautogui.scroll(-240)
-            pyautogui.sleep(0.5)
+            print('scrolling')
+            scroll(-240)
+            sleep(0.5)
             continue
 
         click_center(watch)
-        pyautogui.sleep(0.5)
+        print('watched!')
+        sleep(0.5)
 
     main()
 
